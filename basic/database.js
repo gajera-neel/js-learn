71a5cd227a2274e415f24ff0294f7ca2b9de2db1
gajera-neel <neelgajera3535@gmail.com>
2025-02-12 16:42:10 +0530

    Datatypes

---
+++ basic/database.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) we are using nodejs,not browser
+
+/*
+when you want to best programmer please prefer the ECMAscript website
+AND
+MDn website
+*/
+
+let age=45
+let string="dfg"
+let c = null
+let d;
+console.table([typeof age,typeof string,typeof c,typeof d])
--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,18 @@
-console.log("hi")
+const acid=1234;
+let aemail="jjh@gmail.com"
+var apassword=2546
+acity = "rth"
+let acstate;
+
+aemail="aa@gmail.com"
+apassword=5677
+acity="fff"
+
+console.log(acid)
+console.table([acid,aemail,apassword,acity,acstate])
+
+/*
+  prefer not to use var
+  because of issue in block scope and functional scope
+*/
+
