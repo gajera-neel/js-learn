9a92b844d44bd84f7f2632749417bd1ed6167c21
gajera-neel <neelgajera3535@gmail.com>
2025-02-12 17:09:42 +0530

    conversion of datatype

---
+++ basic/3_conversionOpration.js
@@ -0,0 +1,21 @@
+let score = "33ad"
+
+// console.log(typeof score)
+
+// let we change string datatype into number
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)// ouptput will be a NaN (not a number)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
