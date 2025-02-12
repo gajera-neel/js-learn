587a84876a70596472f65e406c0c2eb1c6ebfb2a
gajera-neel <neelgajera3535@gmail.com>
2025-02-12 17:52:02 +0530

    comparisions

---
+++ basic/4_comparision.js
@@ -0,0 +1,11 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+//check what diference between == and ===, <,>and >= <=;
+
+// strick check ===
+console.log("2" === 2)
